02c1b95f89da867d29b90afa5d8b4a1aa5344c3d
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-12-04 00:22:34 +0800

    Delete remake 3/Potato Shrek Dating Sim/compile.ps1

--- remake 3/Potato Shrek Dating Sim/compile.ps1
+++
@@ -1,3 +0,0 @@
-echo Compiling...; 
-g++ 'Shrek Dating Sim.cpp' -Llib -Wall -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -o 'Shrek Dating Sim'; 
-if ($?) {echo Running...; .\\'Shrek Dating Sim'} 
